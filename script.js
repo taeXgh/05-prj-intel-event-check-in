0186d8b8a46c98877178f2cda68a90ce37307ec3
Thalia Edwards <110435033+taeXgh@users.noreply.github.com>
2025-09-30 01:46:19 +0000

    progress

---
+++ script.js
@@ -0,0 +1,35 @@
+//Get all needed DOM elements
+const form = document.getElementById('checkInForm');
+const nameInput = document.getElementById('attendeeName');
+const teamSelect = document.getElementById('teamSelect');
+
+// Track attendance
+let count = 0;
+const maxCount = 50;
+
+
+// Handle form submission
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    //Get form values
+    const name = nameInput.value;
+    const team = teamSelect.value;
+    const teamName = teamSelect.selectedOptions[0].text;
+
+    console.log(name, teamName);
+
+    // Increment count
+    count++;
+    console.log('Total check-ins: ', count);
+
+    // Update progress bar
+    const percentage = Math.round((count / maxCount) * 100) + "%";
+    console.log(`Progress: ${percentage}`);
+
+    // Update team counter
+    const teamCounter = document.getElementById(team + "Count");
+    teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
+
+    //Show welcome message
+    const message = `🎉`
+  });
